63f5165ae727b5721251f9f3079da76a873864de
NicolePell <pellicena@gmail.com>
2014-11-02 21:35:17 +0000

    16/39 tests passing

--- questions.rb
+++ questions.rb
@@ -55,12 +55,13 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+	string.slice(0..string.length/2)
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
-
+	-number.abs
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
@@ -68,6 +69,9 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+	odd_array = array.select { |num| num.odd? }
+	even_array = array.select { |num| num.even? }
+	array = [even_array, odd_array]
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -79,7 +83,7 @@ end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-
+	array.inject { |memo, word| memo.length < word.length ? memo : word }
 end
 
 # return the shortest word in an array
@@ -90,7 +94,7 @@ end
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
-
+	array.inject { |sum, num| sum + num }
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
